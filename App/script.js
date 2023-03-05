dffec9327b7f90c56d527990ffb0e7e157f56892
Garrett Chavez <garrettachavez@gmail.com>
2023-03-05 15:51:32 -0800

    Several changes
     added -webkit-filter: brightness(0%); to relevant elements
    Fixed me image height issues
    adding transform: translateZ(0); to sticky elements
    Restructured/fixed javascript

--- App/script.js
+++ App/script.js
@@ -1,62 +1,53 @@
-const menuIcon = document.querySelector(".menuIcon");
-const menuCloser = document.querySelector(".menuCloser");
-const menuLinks = document.querySelectorAll(".menu a");
-const menu = document.querySelector(".menu");
+const qs = (selector) => document.querySelector(selector);
+const qsa = (selector) => document.querySelectorAll(selector);
+
+const menu = qs(".menu");
+const menuIcon = qs(".menuIcon");
+const menuCloser = qs(".menuCloser");
+const menuLinks = qsa(".menu a");
 
 const toggleMenu = () => {
-  menuIcon.classList.toggle("active");
-  menuCloser.classList.toggle("active");
-  menu.classList.toggle("active");
+  [menuIcon, menuCloser, menu].forEach((el) => el.classList.toggle("active"));
 };
 
 menuIcon.addEventListener("click", toggleMenu);
 menuCloser.addEventListener("click", toggleMenu);
 menuLinks.forEach((link, index) => {
-  if (index < 4)
-    link.addEventListener("click", toggleMenu);
+  if (index < 4) link.addEventListener("click", toggleMenu);
 });
 
+const setScrollProperty = () => {
+  document.body.style.setProperty("--scroll", window.pageYOffset / 300);
+};
 
-window.addEventListener(
-  "scroll",
-  () => {
-    document.body.style.setProperty("--scroll", window.pageYOffset / 300);
-  },
-  false
-);
+window.addEventListener("scroll", setScrollProperty, false);
 
-const articles = document.querySelectorAll(".contentGrid article");
-let screenMid = window.innerHeight / 2; // calculate the center of the screen
+const articles = qsa(".contentGrid article");
+let screenMid = window.innerHeight / 2;
 
-window.addEventListener("resize", () => {
-  screenMid = window.innerHeight / 2; // recalculate the center of the screen on resize
-});
+const setScreenMid = () => {
+  screenMid = window.innerHeight / 2;
+};
+
+window.addEventListener("resize", setScreenMid);
+
+const setArticleOpacity = (article) => {
+  const bounds = article.getBoundingClientRect();
+  let opacity = 1;
+
+  if ((bounds.y + bounds.height) <= 0 || bounds.y >= window.innerHeight) {
+    opacity = 0.25;
+    const video = article.querySelector("video");
+    if (video) video.pause();
+  } else if ((bounds.y + bounds.height) < screenMid) {
+    opacity = 0.25 + (bounds.y + bounds.height) / screenMid;
+  } else if (bounds.y > screenMid) {
+    opacity = 2.25 - bounds.y / screenMid;
+  }
+
+  article.style.opacity = opacity;
+};
 
 window.addEventListener("scroll", () => {
-  articles.forEach((article) => {
-    if (
-      article.getBoundingClientRect().bottom < 0 ||
-      article.getBoundingClientRect().top > window.innerHeight
-    )
-      return;
-
-    const articleMid =
-      (article.getBoundingClientRect().top +
-        article.getBoundingClientRect().bottom) /
-      2; // get the top position of each article
-    const distance = Math.abs(screenMid - articleMid); // calculate the distance of the article from the center
-    let opacity = 1.50 - distance / screenMid; // calculate the opacity based on the distance
-
-    if (opacity > 1) opacity = 1;
-    // set opacity to 1 if it's greater than 1
-    else if (opacity < 0.25) {
-      opacity = 0.25;
-      const video = article.querySelector('video');
-      if (video) video.pause();
-    }
-    // prevent the opacity from going below 0.25 and pause any video with an opacity of 0.25
-
-
-    article.style.opacity = opacity; // set the opacity of the article
-  });
+  articles.forEach(setArticleOpacity);
 });
